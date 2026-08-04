3cdf79b49aa451594c24e9c2103c969b49b33f23
shubhook <khakhashubham@gmail.com>
2026-08-04 20:21:41 +0530

    fix: suppress hydration warnings from browser extensions
    Added suppressHydrationWarning to html element to prevent false positives
    from extensions like Grammarly that inject attributes (cz-shortcut-listen, etc).
    This doesn't hide real hydration bugs in our code.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col bg-black">{children}</body>
     </html>
